348cf66ea60867a0036949e6102147af4ea949e3
nggoong <nggoong@naver.com>
2021-11-16 17:51:50 +0900

    TodoList 항목 삭제 기능 구현 및 항목 완료를 나타내는 Toggle기능 구현 완료✔

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,10 +2,10 @@ import TodoListItem from "./TodoListItem";
 import './TodoList.scss';
 
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
     return(
         <div className="TodoList">
-            {todos.map(todo=> {return(<TodoListItem todo={todo} key={todo.id}/>)})}
+            {todos.map(todo=> {return(<TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>)})}
         </div>
     )
 }
