b08a51daf371ce05ab19db4614d95455579e456b
nggoong <nggoong@naver.com>
2021-11-16 14:13:46 +0900

    TodoList 구현 및 완료 유무에 따른 UI 디자인 구현 완료✔

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
+import TodoList from './components/TodoList';
+import React, { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text:'리액트의 기초 알아보기',
+      checked:true
+    },
+    {
+      id:2,
+      text:'마트 장 보기',
+      checked:false
+    },
+    {
+      id:3,
+      text:'애플 스토어 갔다오기',
+      checked:true
+    },
+    {
+      id:4,
+      text:'병원 갔다오기',
+      checked:false
+    },
+  ])
+
   return (
     <TodoTemplate>
       <TodoInsert/>
+      <TodoList todos={todos}/>
     </TodoTemplate>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import TodoListItem from "./TodoListItem";
+import './TodoList.scss';
+
+
+const TodoList = ({ todos }) => {
+    return(
+        <div className="TodoList">
+            {todos.map(todo=> {return(<TodoListItem todo={todo} key={todo.id}/>)})}
+        </div>
+    )
+}
+
+
+export default TodoList;
